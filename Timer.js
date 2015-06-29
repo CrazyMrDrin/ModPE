619c1e2939b6d0c5d977b5bd9b8e814a549283cd
CrazyMrDrin@gmail.com <cheatengine6.2>
2015-06-30 00:34:20 +0400

    create project

---
+++ Timer.js
@@ -0,0 +1,9 @@
+﻿var time = 100;// Time //Время
+var dtime = 100; //Default time //Время по умолчанию
+function modTick(){ //Hook ModTick //Хук ModTick
+	time--; 
+	if(time==0){
+		//Your code here //Твой код здесь
+		time=dtime;
+	}
+}
